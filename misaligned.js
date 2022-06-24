39af8a7b32e908169e810ccf8118efbb910b4450
Revathi Shanmugaboopathi <EHR1COB@rbeigcn.com>
2022-06-24 10:15:20 +0530

    intial commit

--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,25 @@
-const {expect} = require('chai')
+const { expect } = require("chai");
 
 function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
-        }
+  const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+  const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+  for (let i = 0; i < majorColors.length; i++) {
+    for (let j = 0; j < minorColors.length; j++) {
+      console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
     }
-    return majorColors.length * minorColors.length;
+  }
+  return majorColors.length * minorColors.length;
 }
 
-result = print_color_map();
+function lengthOfColors() {
+  const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+  const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+  return majorColors.length * minorColors.length;
+}
+result = lengthOfColors();
 expect(result).equals(25);
-console.log('All is well (maybe!)');
+expect(result).equals(25);
+result = print_color_map();
+
+//Add negative scenario
+console.log("All is well (maybe!)");
